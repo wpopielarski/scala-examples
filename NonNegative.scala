c9c355353170e19d7cdc4e1b0b0a2fdfbf19cb61
wpopielarski <wpopielarski@gmail.com>
2016-11-14 08:55:56 +0100

    Small improvements

--- NonNegative.scala
+++ NonNegative.scala
@@ -23,13 +23,13 @@ object NonNegativeAlgebra {
   def apply[T, U <: _ @@ NonNegative](from: T)(constr: T => Option[U]): Option[U] = constr(from)
   // implicit postfix to infix
   // explicit version
-//  implicit class addAlgebra[T, U <: _ @@ NonNegative](t: U)(implicit ev: NonNegativeAlgebra[T, U]) {
-//    def |+|(s: T): Option[U] = ev.|+|(t, s)
-//    def |-|(s: T): Option[U] = ev.|-|(t, s)
-//  }
+  //  implicit class addAlgebra[T, U <: _ @@ NonNegative](t: U)(implicit ev: NonNegativeAlgebra[T, U]) {
+  //    def |+|(s: T): Option[U] = ev.|+|(t, s)
+  //    def |-|(s: T): Option[U] = ev.|-|(t, s)
+  //  }
 
   // implicit version
-  implicit class addAlgebraImplicit[T, U <: _ @@ NonNegative : ({ type Alg[C] = NonNegativeAlgebra[T, C] })#Alg](t: U) {
+  implicit class addAlgebraImplicit[T, U <: _ @@ NonNegative: ({ type Alg[C] = NonNegativeAlgebra[T, C] })#Alg](t: U) {
     def |+|(s: T): Option[U] = implicitly[NonNegativeAlgebra[T, U]].|+|(t, s)
     def |-|(s: T): Option[U] = implicitly[NonNegativeAlgebra[T, U]].|-|(t, s)
   }
@@ -38,20 +38,19 @@ object NonNegativeAlgebra {
 object NonNegativeTest {
   import NonNegativeAlgebra._
   // when double conveys non negative value
+  // constructor
+  def unit(k: Double) = NonNegativeAlgebra[Double, Double @@ NonNegative](k) { k =>
+    if (k < 0.0) None
+    else Option(tag(k))
+  }
+  // non negative algebra for double
   implicit val doubleAlgebra = new NonNegativeAlgebra[Double, Double @@ NonNegative] {
-    def |+|(s1: Double @@ NonNegative, s2: Double) = Option(tag(s1 + s2))
-    def |-|(s1: Double @@ NonNegative, s2: Double) = {
-      val r = s1 - s2
-      if (r < 0.0) None else Option(tag(r))
-    }
+    def |+|(s1: Double @@ NonNegative, s2: Double) = unit(s1 + s2)
+    def |-|(s1: Double @@ NonNegative, s2: Double) = unit(s1 - s2)
   }
 
   // just test
   def test(k: Double): Option[Double @@ NonNegative] = {
-    def unit(k: Double) = NonNegativeAlgebra[Double, Double @@ NonNegative](k) { k =>
-      if (k < 0.0) None
-      else Option(tag(k))
-    }
     val one = unit(k).flatMap { _ |+| 5.0 }.flatMap { _ |-| 3.0 }
     val two = tag[Double, NonNegative](5.0)
     two |-| 2.0 flatMap { t => one.flatMap { o => unit(o + t) } }
